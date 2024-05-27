70f154e88ca035fa5682bed065d53c096a3d1baa
조준희 <iopp3423@gmail.com>
2024-05-27 15:14:15 +0900

    [add] postEntity 기본 컬럼 설정

---
+++ src/module/post/entity/post.entity.ts
@@ -0,0 +1,78 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { number } from 'joi';
+import { text } from 'express';
+
+export class Post {
+  @PrimaryGeneratedColumn()
+  @ApiProperty({ description: 'id' })
+  id: number;
+
+  @Column()
+  @ApiProperty({
+    type: String,
+    example: '제목',
+  })
+  title: string;
+
+  @Column()
+  @ApiProperty({
+    type: number,
+    example: '문제번호',
+  })
+  problem_number: number;
+
+  @Column()
+  @ApiProperty({
+    type: String,
+    example: '문제링크',
+  })
+  problem_link: string;
+
+  @Column()
+  @ApiProperty({
+    type: number,
+    example: '중요도',
+  })
+  rate: number;
+
+  @Column()
+  @ApiProperty({
+    type: text,
+    example: '내용',
+  })
+  content: string;
+
+  @Column()
+  @ApiProperty({
+    type: number,
+    example: '난이도',
+  })
+  difficulty: string;
+
+  @Column()
+  @ApiProperty({
+    type: Date,
+    example: '알림받을날짜',
+  })
+  alarm: Date;
+
+  @CreateDateColumn()
+  @ApiProperty({
+    type: Date,
+    example: '생성일자',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @ApiProperty({
+    type: Date,
+    example: '수정일자',
+  })
+  updatedAt: Date;
+}
