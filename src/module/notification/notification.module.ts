dec37e2d71a94afc4c15de99aa65178b6fc22089
조준희 <iopp3423@gmail.com>
2024-07-16 12:18:09 +0900

    update: redis 호스트 환경변수 설정

--- src/module/notification/notification.module.ts
+++ src/module/notification/notification.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from "./service/notification.service";
-import { ConfigService } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { BullModule } from "@nestjs/bull";
 import { NotificationProcessor } from "./processor/notification.processor";
 
 @Module({
   imports:[
-    BullModule.forRoot({
-      redis:{
-        host: 'localhost',
-        port: 6379,
-      },
+    ConfigModule.forRoot(),
+    BullModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory : async(configService: ConfigService) => ({
+        redis:{
+          host: configService.get<string>('REDIS_HOST') || 'localhost',
+          port: configService.get<number>('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
     }),
     BullModule.registerQueue({
       name: 'notification',
